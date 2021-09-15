d48d6fe6f3dff85701da43295a3187cb6db017e5
Maki Aboabida <maki-miko@hotmail.com>
2021-09-15 15:30:42 -0400

    ignore commit (getting help for error)

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+
+
 const inquirer = require('inquirer')
 
 // FIRST PART TO PROMPT 
@@ -133,15 +139,10 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-  });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
+  const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw err;
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+  });
